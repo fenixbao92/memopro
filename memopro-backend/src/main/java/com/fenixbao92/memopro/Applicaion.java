7d697abf347f329b1531c4535eb3c0c18c7cbb6d
fenixbao92 <fenixbao92@gmail.com>
2019-04-24 12:36:41 +0800

    project init.

---
+++ memopro-backend/src/main/java/com/fenixbao92/memopro/Applicaion.java
@@ -0,0 +1,14 @@
+package com.fenixbao92.memopro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Applicaion {
+
+    public static void main(String[] args){
+        SpringApplication.run(Applicaion.class, args);
+        System.out.println("启动成功");
+    }
+
+}
