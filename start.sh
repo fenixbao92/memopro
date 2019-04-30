e5b4206f886f25cfbab7c6331f897d8295fc0a43
fenixbao92 <fenixbao92@gmail.com>
2019-04-30 21:51:32 +0800

    fixbug

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@ cd memopro-frontend
 npm install
 npm run build
 mkdir ../memopro-backend/src/main/resources/static
-cp ./dist/* ../memopro-backend/src/main/resource/static
+cp ./dist/* ../memopro-backend/src/main/resources/static
 cd ../memopro-backend
 mvn install
 nohup java -jar ./target/memopro-backend-1.0-SNAPSHOT.jar &
