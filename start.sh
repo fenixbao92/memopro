4d1ed21730fc172cad7e2d16311f5c95438113bf
fenixbao92 <fenixbao92@gmail.com>
2019-05-08 06:16:02 +0800

    update setup scripts.

--- start.sh
+++ start.sh
@@ -5,4 +5,5 @@ mkdir ../memopro-backend/src/main/resources/static
 cp -r ./dist/* ../memopro-backend/src/main/resources/static
 cd ../memopro-backend
 mvn install
+jps | grep memopro-backend | awk '{print $1}' | xargs kill -9
 nohup java -jar ./target/memopro-backend-1.0-SNAPSHOT.jar &
