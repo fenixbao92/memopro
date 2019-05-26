e249bdc4d88d45f732b202f62e0fce66240ab9a0
fenixbao92 <fenixbao92@gmail.com>
2019-05-27 01:07:07 +0800

    wx auth deal.

--- memopro-backend/src/main/java/com/fenixbao92/memopro/controller/proxy/WxProxyController.java
+++ memopro-backend/src/main/java/com/fenixbao92/memopro/controller/proxy/WxProxyController.java
@@ -1,5 +1,10 @@
 package com.fenixbao92.memopro.controller.proxy;
 
+import com.fenixbao92.memopro.common.constants.BusinessExceptionEnum;
+import com.fenixbao92.memopro.common.exceptions.BusinessException;
+import com.fenixbao92.memopro.common.model.wx.WxUser;
+import com.fenixbao92.memopro.common.utils.UserUtil;
+import com.fenixbao92.memopro.common.vo.wx.WxRegisterVo;
 import com.fenixbao92.memopro.service.wx.WxService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,16 +13,39 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
+//wxproxy is open api with no auth.
 @RestController
 @RequestMapping("/wxproxy")
 public class WxProxyController {
 
     @Resource
-    WxService wxService;
+    private WxService wxService;
 
     @RequestMapping("/codeAuth")
     public String codeAuth(@RequestParam String code) {
         return wxService.codeAuth(code);
+    }
+
+    @RequestMapping(value = "/register")
+    public void register(WxRegisterVo wxRegisterVo, HttpServletRequest httpServletRequest) {
+        String sessionId = httpServletRequest.getHeader("sessionId");
+        wxRegisterVo.setSessionId(sessionId);
+        wxService.register(wxRegisterVo);
+    }
+
+    @RequestMapping("/getbindinfo")
+    public WxUser getbindinfo(HttpServletRequest httpServletRequest) {
+        String sessionId = httpServletRequest.getHeader("sessionId");
+        return wxService.getbindinfo(sessionId);
+    }
+
+    @RequestMapping(value = "/test")
+    public String test(Integer c) {
+        if(c==1){
+            throw new BusinessException(BusinessExceptionEnum.WX_SESSION_INVALID);
+        }
+        String accountofCurrentUser = UserUtil.getAccountofCurrentUser();
+        return "test success";
     }
 
 }
