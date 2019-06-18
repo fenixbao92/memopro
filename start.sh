b700b0ca86877d701d75b782a352554e56a2d6c9
fenixbao92 <fenixbao92@gmail.com>
2019-06-18 21:50:34 +0800

    update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,4 @@
 jps | grep memopro-backend | awk '{print $1}' | xargs kill -9
-cd memopro-frontend
-npm install
-npm run build
-rm -rf ../memopro-backend/src/main/resources/static
-mkdir ../memopro-backend/src/main/resources/static
-cp -r ./dist/* ../memopro-backend/src/main/resources/static
 cd ../memopro-backend
 mvn install
 nohup java -jar ./target/memopro-backend-1.0-SNAPSHOT.jar &
